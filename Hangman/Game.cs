621ecb423f4bec3faead49f9f9b5deb8457f5bf4
Erik Rossi <erikdenfredlige@gmail.com>
2014-12-13 00:25:04 +0100

    Initial release.

---
+++ Hangman/Game.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman
+{
+    public class Game
+    {
+
+        public int lives;
+        public string word;
+        public int whichWord;
+        
+        public void setWord()
+        {
+            Random wordDeterminer = new Random();
+            whichWord = wordDeterminer.Next(1, 5);
+
+            SqlConnection connectionToDB = new SqlConnection
+                ("Data Source=MONARKEN;Initial Catalog=HangmanDB;Integrated Security=True");
+            
+            SqlCommand query = new SqlCommand("SELECT word FROM words where ID = " + whichWord, connectionToDB);
+
+            connectionToDB.Open();
+
+            word = (string)query.ExecuteScalar();
+        }
+
+        public void setLives()
+        {
+            lives = 3;
+        }
+
+        public string getWord()
+        {
+            return word;
+        }
+
+    }
+}
